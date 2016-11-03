8a8e58b70560417f9d43e161619abbf9189e9fd7
Sukhdeep <sukhdeep@360itpro.com>
2016-11-03 14:17:53 +0530

    new demo added

---
+++ demo3/demo3/DemoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DemoViewController.swift
+//  demo3
+//
+//  Created by Sukhdeep Singh on 03/11/16.
+//  Copyright © 2016 Mobilyte. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     print("new demo added")
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
